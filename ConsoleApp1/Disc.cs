44f7d21c48993690faab71b8cae50b90daa3ef1b
Иванов Максим Николаевич <m.ivanov@analit.net>
2020-02-14 15:44:04 +0300

    Add project files.

---
+++ ConsoleApp1/Disc.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace Intervals
+{
+    class Disc
+    {
+        private long _left { get; set; }
+        private long _right { get; set; }
+
+        public static int Func(int[] A)
+        {
+            var discs = new Disc[A.Length];
+            for (int i = 0; i < A.Length; i++)
+                discs[i] = new Disc()
+                {
+                    _left = i - A[i],
+                    _right = i + A[i]
+                };
+            discs = discs.OrderBy(i => i._left).ToArray();
+            int sum = 0;
+            for (int i = 0; i < discs.Length; i++)
+            {
+                var right = discs[i]._right;
+                for (int j = i + 1; j < discs.Length && discs[j]._left <= right; j++)
+                    sum++;
+            }
+            return sum;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Intervals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = Input();
+            if (A == null)
+            {
+                Console.WriteLine("Please, enter integers");
+                return;
+            }
+            var res = Disc.Func(A);
+            Console.WriteLine(res);
+            Console.Read();
+        }
+
+        static int[] Input()
+        {
+            var input = Console.ReadLine().Split();
+            int[] temp = new int[input.Length];
+            for (int i = 0; i < input.Length; ++i)
+                if (!int.TryParse(input[i], out temp[i]))
+                {
+                    return null;
+                }
+            return temp;
+        }
+    }        
+}
